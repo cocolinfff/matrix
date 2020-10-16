c4394d05fce076a3049e854994243e6416d03b9b
Colin <34176582+cocolinfff@users.noreply.github.com>
2020-10-16 20:54:31 +0800

    Revert "Changed almost nothing"

--- matrix.cpp
+++ matrix.cpp
@@ -2,19 +2,19 @@
 #include"matrix.h"
 #include"fx_matrix.h"
 template<class T>
-matrix<T> matrix<T>::mean() {
-	T sum;
-	matrix<T> new_matrix(1, 1, 0);
-	for (int i = 0; i < this->x; i++) {
-		for (int j = 0; j < this->y; j++) {
-			sum += this->data[i][j];
-		}
-	}
-	new_matrix.data[0][0] = (T)(sum / (this->x * this->y));
-	new_matrix.leaf[0] = this;
-	new_matrix.leaf_way = MEAN;
-	return new_matrix;
-}
+  matrix<T> matrix<T>::mean(){
+        T sum;
+        matrix<T> new_matrix(1,1,0);
+        for (int i = 0; i < this->x;i++){
+            for (int j = 0; j < this->y;j++){
+                sum += this->data[i][j];
+            }
+        }
+        new_matrix.data[0][0] = (T)(sum/(this->x*this->y));
+        new_matrix.leaf[0] = this;
+        new_matrix.leaf_way = MEAN;
+        return new_matrix;
+    }
 
 
 
